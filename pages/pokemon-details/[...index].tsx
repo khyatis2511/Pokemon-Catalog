850f4bee91922f61ceb8c54ec4b809e0b267c6a1
khyati2511 <khushisoni2511@gmail.com>
2023-04-18 22:02:11 +0530

    Implementation for pokeman details and pokeman evloution popup

--- pages/pokemon-details/[...index].tsx
+++ pages/pokemon-details/[...index].tsx
@@ -1,30 +1,149 @@
+/* eslint-disable @next/next/no-img-element */
 import client from "@/utils/apollo-client";
 import { pokemonType } from "@/utils/types";
 import { gql } from "@apollo/client";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ParsedUrlQuery } from "querystring";
+import PokemonList from "@/View/PokemonList";
 
 interface Params extends ParsedUrlQuery {
   slug: string;
 }
 
 interface PokemonDetailsProps {
-  pokemonData: pokemonType[],
+  pokemon: pokemonType,
 }
 
-const PokemonDetails : FC<PokemonDetailsProps> = ({pokemonData}) => {
+const PokemonDetails : FC<PokemonDetailsProps> = ({pokemon}) => {
+  console.log('pokemon data : ', pokemon)
+
+  const [evloutionData, setEvloutionData] = useState<pokemonType[]>([]);
+  const [showPopup, setShowPopup] = useState(false);
   const router = useRouter();
+
+  
+  const getPokemanEvolutions = async () => {
+    const {id, name} = pokemon
+
+    console.log(id, name);
+    const fragment = gql `
+      fragment RecursivePokemonFragment on Pokemon {
+        id
+        number
+        name
+        weight{
+          minimum
+          maximum
+        }
+        height{
+          minimum
+          maximum
+        }
+        classification
+        types
+        resistant
+        weaknesses
+        fleeRate
+        maxCP
+        maxHP
+        image
+      }
+    `
+    const { data } = await client.query({
+      query: gql`
+      query pokemon {
+        pokemon(id: "${id}", name: "${name}"){
+          id
+          name
+          evolutions{
+            id
+            number
+            name
+            classification
+            types
+            resistant
+            weaknesses
+            fleeRate
+            maxCP
+            evolutions{
+              ...RecursivePokemonFragment
+            }
+            maxHP
+            image
+          }
+        }
+      }
+      ${fragment}
+      `,
+    });
+    setEvloutionData(data.pokemon.evolutions);
+    setShowPopup(true);
+    // console.log('evloution dta:', data);
+  }
+
+  console.log(evloutionData);
+
   if (router.isFallback) {
     return (
       <h1>Loading</h1>
     )
   }
+
+  if(!pokemon || Object.keys(pokemon).length === 0) {
+    return (
+      <h1>Data not found </h1>
+    )
+  }
+  
   return (
+    <>
     <div>
-        <h1>123</h1>
+      <img 
+        src={pokemon.image} 
+        alt={pokemon.name} 
+        title={pokemon.name} 
+      />
+      <div>
+        <h3>{pokemon.name}</h3>
+        <p>maximum height : {pokemon.height.maximum}</p>
+        <p>maximum weight : {pokemon.weight.maximum}</p>
+        <p>Classification : {pokemon.classification}</p>
+        <div>
+          <h2>Type</h2>
+          {pokemon.types.map((type) => (
+            <p key={type}>{type}</p>
+          ))}
+        </div>
+        <div>
+          <h2>Resistant</h2>
+          {pokemon.resistant.map((resistant1) => (
+            <p key={resistant1}>{resistant1}</p>
+          ))}
+        </div>
+        <div>
+          <h2>Weaknesses</h2>
+          {pokemon.weaknesses.map((weakness) => (
+            <p key={weakness}>{weakness}</p>
+          ))}
+        </div>
+        <button 
+          type="button"
+          onClick={getPokemanEvolutions}
+        >
+          Pokemon evolutions
+        </button>
+      </div>
     </div>
+    {showPopup && <div>
+      <button type="button" onClick={() => setShowPopup(false)}>Close</button>
+      {evloutionData.length !== 0 ? 
+        <PokemonList pokemonData={evloutionData} /> : 
+        <p>Data not found.</p>
+      }
+    </div>}
+    </>
   );
 }
 
@@ -78,7 +197,7 @@ export const getStaticPaths : GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({params}) => {
   let id;
   let name;
-  let pokemonData = [];
+  let pokemonData;
   try {
     const {index} = params as Params;
     if(index) {
@@ -88,7 +207,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
     if(id && name) {
       const { data } = await client.query({
         query: gql`
-        query pokemon{
+        {
           pokemon(id: "${id}", name: "${name}"){
             id
             number
@@ -118,10 +237,12 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   } catch (error) {
     console.log('error', error);
   }
+
+  const { pokemon } = pokemonData;
   
   return {
     props: {
-      pokemonData: pokemonData || null,
+      pokemon: pokemon || null,
     },
   };
 }
